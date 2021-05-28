7b5b5316a3726ddc9b8d8d5b1460dab1165ccab7
Erickson Siqueira da Silva <83774878+EricksonSiqueira@users.noreply.github.com>
2021-05-28 17:53:31 -0300

    Primeiro exercicio, usando const e let
    Aprendendo também a declarar variável dentro de console.log com ${variável}

---
+++ Basico/Exercicios/ex1.js
@@ -0,0 +1,12 @@
+const nome = 'Luiz'
+const sobrenome = 'Otávio Miranda'
+let idade = 32;
+let peso = 84;
+let alturaEmM = 1.8
+let imc = peso/(alturaEmM**2)
+
+let anoNascimento = 2021-idade
+
+console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso}KG`)
+console.log(`Tem ${alturaEmM}m de altura e seu IMC é de  ${imc.toFixed(2)}`)
+console.log(`${nome} nasceu em ${anoNascimento}.`)
