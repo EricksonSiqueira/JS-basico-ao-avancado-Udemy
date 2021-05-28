fc520f93f6b58d8a0eb3d932c018432b4ce8c195
Erickson Siqueira da Silva <83774878+EricksonSiqueira@users.noreply.github.com>
2021-05-28 17:36:46 -0300

    const e typeof

---
+++ Basico/aula-7 (constantes)/script.js
@@ -0,0 +1,18 @@
+const nome = 'Erickson'
+console.log(nome)
+
+//const nome precisa começar com um valor, se não da erro
+// nome = 'Otávio' não posso modificar ese valor
+
+const primeiroNumero = 5
+const segundoNumero = 10
+const conta = primeiroNumero*segundoNumero
+
+console.log(conta)
+const resultadoDuplicado = conta*2
+console.log(resultadoDuplicado)
+const resultadoTriplicado = conta*3
+console.log(resultadoTriplicado)
+
+console.log(typeof(segundoNumero))
+//typeof mostra o tipo da variável 
