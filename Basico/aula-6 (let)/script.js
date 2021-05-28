e24299cd512a2b3da8d34fe80b159337eb463e09
Erickson Siqueira da Silva <83774878+EricksonSiqueira@users.noreply.github.com>
2021-05-28 17:19:17 -0300

    variavel let
    sempre use let e nunca var

---
+++ Basico/aula-6 (let)/script.js
@@ -0,0 +1,27 @@
+let nome = 'João'
+
+console.log(nome, ' nasceu em 1984.')
+console.log('Em 200 ', nome, ' conheceu Maria.')
+console.log(nome, 'casou-se com Maria em 2012.')
+console.log('Maria teve um filho com ', nome, ' em 2015.')
+console.log('O filho de', nome,'se chama Eduardo.')
+
+let nome1; //indefinida
+console.log(nome1)
+
+nome1 = 'Erickson' //atribui o valor
+console.log(nome1)
+
+nome1 = 'Anna'
+console.log(nome1)
+
+//Não se pode criar variáveis com palavras reservadas por ex let let ou let if
+//Variáveis precisam ter nomes significativos, para deixar o código mais claro
+//Não se pode começar o nome de uma variável com número
+//É bom começar com letra minuscula
+//Não se pode conter espaço ou traços
+//Utilizar camel case
+//JavaScript é case sensitivy
+//Não podemos redeclarar uma variável let
+//Não use var, USE LET. Var é antigo.
+
