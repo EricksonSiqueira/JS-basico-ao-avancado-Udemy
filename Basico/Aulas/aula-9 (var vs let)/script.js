13dab566e9eef3cce7eab89a5428b6711075c39f
Erickson Siqueira da Silva <83774878+EricksonSiqueira@users.noreply.github.com>
2021-05-29 00:56:53 -0300

    Var gera problemas que o let n gera

---
+++ Basico/Aulas/aula-9 (var vs let)/script.js
@@ -0,0 +1,11 @@
+// ECMAScript 2015 (ES6)
+var nome = 'Luiz'
+var nome = "Otávio" //da pra declarar a variável 2 vezes
+
+console.log(nome)
+
+nome = 'Luiz'
+
+console.log(nome)
+
+// Sempre USE LET ou const nunca var
