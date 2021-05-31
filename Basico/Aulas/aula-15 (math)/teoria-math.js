59bba88f348c1ad3c033735f60cb7600d2ff275b
Erickson Siqueira da Silva <83774878+EricksonSiqueira@users.noreply.github.com>
2021-05-31 19:28:47 -0300

    Algumas funções do objeto Math

---
+++ Basico/Aulas/aula-15 (math)/teoria-math.js
@@ -0,0 +1,18 @@
+let num1 = 9.5412938
+let num2 = Math.floor(num1) // arredondando pra baixo
+let num3 = Math.ceil (num1) // arredondando pra cima
+
+console.log(num2)
+console.log(num3)
+console.log(Math.round(num1)) //round arredonda o número automáticamente
+
+console.log(Math.max(1,2,4,5,6,-10,1500,9,6,14,412,4)) //pega o maior número da sequência
+
+console.log(Math.min(1,2,4,5,6,-10,1500,9,6,14,412,4)) //pega o menor número da sequência
+
+let aleatorio = Math.random()*(10 - 5) *5 //número aleatório entre 5-10
+console.log(aleatorio)
+
+console.log(Math.PI)
+
+console.log(100/0) // da infinito e não indefinido(calculo 1 feelings)
