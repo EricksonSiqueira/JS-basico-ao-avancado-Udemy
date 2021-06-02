95a383a016e26c42ea2bb0d04136330d1da614b4
Erickson Siqueira da Silva <83774878+EricksonSiqueira@users.noreply.github.com>
2021-06-02 11:12:08 -0300

    Operadores de comparação
    nunca usar == somente ===
    mesma coisa vale para != e !==

---
+++ Logica-de-programacao/Aulas/aula-21/script.js
@@ -0,0 +1,17 @@
+/**
+ * Operadores de comparação
+ * 
+ * > maior que
+ * >= maior que ou igual a
+ * < menor que
+ * <= menor que ou igual a
+ * == igualdade  ************** não recomendado
+ * ==== igualdade estrita (valor e tipo)
+ * != diferente (valor) ******* não recomendado
+ * !== diferente estrito (valor e tipo)
+ */
+const num1 = 10
+const num2 = 11
+const numFake = '10'
+const expressao = num1 === numFake
+console.log(expressao);
