4d62c6e5990837c7fcc40d15f4f5433de44ee9ae
Erickson Siqueira da Silva <83774878+EricksonSiqueira@users.noreply.github.com>
2021-06-02 13:05:01 -0300

    Entendendo mais um pouco de if, else if e else

---
+++ Logica-de-programacao/Aulas/aula-25 (if,else if, else 2)/teoria.js
@@ -0,0 +1,11 @@
+const numero = 14
+
+if (numero >= 0 && numero <= 5){
+    console.log("Número está entre 0 e 5")
+} else if (numero > 5 && numero <= 10) {
+    console.log("Número está entre 6 e 10")
+} else if (numero > 10 && numero <= 15){
+    console.log("Número está enrte 11 e 15")
+} else {
+    console.log("Número não etá enrte 0 e 15")
+}
