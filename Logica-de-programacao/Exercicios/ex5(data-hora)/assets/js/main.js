c75aec45ebc87885cc7278daa3da512112f9c00c
Erickson Siqueira da Silva <83774878+EricksonSiqueira@users.noreply.github.com>
2021-06-03 15:32:31 -0300

    Função getMonthTxt
    Essa função pega o mês atual e retorna o seu valor em texto.

--- Logica-de-programacao/Exercicios/ex5(data-hora)/assets/js/main.js
+++ Logica-de-programacao/Exercicios/ex5(data-hora)/assets/js/main.js
@@ -1,10 +1,11 @@
-function setH1 (texto) {
+const data = new Date()
+
+function setH1(texto) {
     const h1 = document.querySelector('.data-hora')
     h1.innerHTML = ''
     h1.innerHTML = texto
 }
-function getDayWeek (){
-    const data = new Date()
+function getDayWeek() {
     const diaSemana = data.getDay()
 
     switch (diaSemana) {
@@ -18,7 +19,7 @@ function getDayWeek (){
             return 'Quarta-feira'
         case 4:
             return 'Quinta-feira'
-        case 5: 
+        case 5:
             return 'Sexta-feira'
         case 6:
             return 'Sábado'
@@ -27,4 +28,38 @@ function getDayWeek (){
     }
 
 }
-setH1(`${getDayWeek()}`)
+
+function getMonthTxt() {
+    const mes = data.getMonth()
+
+    switch (mes) {
+        case 0:
+            return 'Janeiro'
+        case 1:
+            return 'Fevereiro'
+        case 2:
+            return 'Março'
+        case 3:
+            return 'Abril'
+        case 4:
+            return 'Maio'
+        case 5:
+            return 'Junho'
+        case 6:
+            return 'Julho'
+        case 7:
+            return 'Agosto'
+        case 8:
+            return 'Setembro'
+        case 9:
+            return 'Outubro'
+        case 10:
+            return 'Novembro'
+        case 11:
+            return 'Dezembro'
+        default:
+            return -1
+    }
+
+}
+setH1(`${getDayWeek()}, ${getMonthTxt()}`)
