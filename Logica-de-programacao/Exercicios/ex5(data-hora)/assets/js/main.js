4488d57c6f2fb4be1f956faa0ae1185a4b957aa0
Erickson Siqueira da Silva <83774878+EricksonSiqueira@users.noreply.github.com>
2021-06-03 15:22:16 -0300

    Função getDayWeek
    Pega o dia da semana e retorna o texto correspondente.

--- Logica-de-programacao/Exercicios/ex5(data-hora)/assets/js/main.js
+++ Logica-de-programacao/Exercicios/ex5(data-hora)/assets/js/main.js
@@ -1,5 +1,30 @@
 function setH1 (texto) {
     const h1 = document.querySelector('.data-hora')
+    h1.innerHTML = ''
     h1.innerHTML = texto
 }
-setH1("abudabi")
+function getDayWeek (){
+    const data = new Date()
+    const diaSemana = data.getDay()
+
+    switch (diaSemana) {
+        case 0:
+            return 'Domingo'
+        case 1:
+            return 'Segunda-feira'
+        case 2:
+            return 'Terça-feira'
+        case 3:
+            return 'Quarta-feira'
+        case 4:
+            return 'Quinta-feira'
+        case 5: 
+            return 'Sexta-feira'
+        case 6:
+            return 'Sábado'
+        default:
+            return -1
+    }
+
+}
+setH1(`${getDayWeek()}`)
