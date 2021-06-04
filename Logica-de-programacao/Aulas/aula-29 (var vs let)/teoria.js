8dc98348fbae9e384eecb0e0130aad609634667d
Erickson Siqueira da Silva <83774878+EricksonSiqueira@users.noreply.github.com>
2021-06-04 18:51:59 -0300

    Diferenças entre let e code
    Nunca use var, deixa usar variavel antes de delcarar. Use sempre let .

---
+++ Logica-de-programacao/Aulas/aula-29 (var vs let)/teoria.js
@@ -0,0 +1,35 @@
+/*const verdadeira = true
+
+// let tem escopo de bloco (...bloco)
+//Var só tem escopo de função
+ 
+let nome = "Samuca"
+var nome2 = "Erisu"
+
+
+
+if (verdadeira) {
+    var nome2 = "Erisu" // Redeclarando
+    let nome = 'Tom' // criando
+
+
+    if(verdadeira) {
+        let nome = "Outra coisa"
+        
+        var nome2 = 'Edesio' //redeclarando
+        console.log(nome, nome2)
+    }
+}
+
+function falaOi(){
+    var nome3 = 'Abudabi' //bloco de função protege a variável
+    console.log(nome3)
+}
+falaOi()*/
+
+const func = function () { 
+    console.log ('Abudabi')
+}
+
+console.log(sobrenome)
+var sobrenome = 'Brito' // deixa usar a variável antes de ser declarada wtf o let nao deixa
