0cb3c1fdae9b7f00b11066726c8d2dd6b6f85c10
Erickson Siqueira da Silva <83774878+EricksonSiqueira@users.noreply.github.com>
2021-06-05 19:41:57 -0300

    atribuição por desestruturação
    utilizando com arrays, bem util

--- Logica-de-programacao/Aulas/aula-30(atribuicao via desestruturacao)/teoria.js
+++ Logica-de-programacao/Aulas/aula-30(atribuicao via desestruturacao)/teoria.js
@@ -1,6 +1,18 @@
-let a = 'A'
-let b = 'B'
+const numeros = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000]
 
-[a, b] = [1, 2]
+const [um, ,tres, ,cinco, ...resto] = numeros; 
+// pega os indices em ordem
+// ... pega o que sobrar dentro do array (rest operator)
+//... pode ser spread operator
+// posso pular indices fazendo , ,
 
-console.log(a, b)
+console.log(um, tres, cinco)
+
+const num = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+console.log(num[1][2])
+const [,[,,seis]] = num
+console.log(seis)
