9d37964518133095fa12f24c03bc54bc1b80f484
Erickson Siqueira da Silva <83774878+EricksonSiqueira@users.noreply.github.com>
2021-06-08 11:45:15 -0300

    Criando vetor de elementos do exercício

---
+++ Logica-de-programacao/Exercicios/ex6(for)/assets/js/main.js
@@ -0,0 +1,7 @@
+const elements = [ //vetor com os objetos que quero que sejam adicionados
+    {tag:'p', texto:'Frase1'},
+    {tag:'div', texto: 'Frase2'},
+    {tag:'footer', texto: 'Frase3'},
+    {tag: 'section', texto: 'Frase4'}
+]
+
