71c211da730ad5597ba12e5798b1956a867fd0ea
Erickson Siqueira da Silva <83774878+EricksonSiqueira@users.noreply.github.com>
2021-06-08 12:13:43 -0300

    Ajustando o exercício
    Criando a div para jogar todos os elementos dentro dela.

--- Logica-de-programacao/Exercicios/ex6(for)/assets/js/main.js
+++ Logica-de-programacao/Exercicios/ex6(for)/assets/js/main.js
@@ -4,6 +4,10 @@ const elements = [ //vetor com os objetos que quero que sejam adicionados
     {tag:'footer', texto: 'Frase3'},
     {tag: 'section', texto: 'Frase4'}
 ]
+const container = document.querySelector('.container')
+const div = document.createElement('div')
+div.classList.add('div1')
+container.appendChild(div)
 
 function addElement (elemento, local){ // pega um elemento com tag e texto e joga no html
     const container = document.querySelector(local)
@@ -12,5 +16,6 @@ function addElement (elemento, local){ // pega um elemento com tag e texto e jog
 }
 
 for (let i = 0; i < elements.length; i++){ // pega todos os elementos do vetor elements
-    addElement(elements[i],'.container')
+    addElement(elements[i],'.div1')
 }
+
