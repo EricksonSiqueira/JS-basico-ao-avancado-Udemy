187cfce8de17bd5128e00c215809e8653c80b354
Erickson Siqueira da Silva <83774878+EricksonSiqueira@users.noreply.github.com>
2021-06-09 14:25:28 -0300

    Modo paisagem
    Função que checa se está ou não em modo paisagem. Com arrow function.

---
+++ Logica-de-programacao/Exercicios/ex9(modo paisagem)/script.js
@@ -0,0 +1,5 @@
+// escreva uma função chamada e paisagem que recebe dois argumentos, largura e altura de uma imagem(number).
+// retorna true se a imagem estiver no modo paisagem
+
+const ePaisagem = (largura, altura) => largura > altura ? true : false
+console.log(ePaisagem(1920, 1080))
