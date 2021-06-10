41caea770809d756fcfb3223d938f0e30f198b2a
Erickson Siqueira da Silva <83774878+EricksonSiqueira@users.noreply.github.com>
2021-06-10 13:23:32 -0300

    setInterval e setTimeOut
    setInterval executa uma função sem parar com o intervalo de tempo passado por parâmetro.
    setTimeOut executa uma função pelo tempo passado em parâmetro.

---
+++ Logica-de-programacao/Aulas/aula-38(setInterval e setTimeout)/teoria.js
@@ -0,0 +1,24 @@
+function mostraHora() {
+    let data = new Date()
+
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false,
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+    })
+}
+
+
+
+const timer = setInterval(function () { // repete a função no intervalo passado, corre infinitamente
+    console.log(mostraHora())
+}, 1000)
+
+setTimeout(function(){ // setTimeOut executa a função pelo tempo passado por parâmetro
+    clearInterval(timer)
+},3000)
+
+setTimeout(function() {
+    console.log('Olá mundo')
+},5000)
