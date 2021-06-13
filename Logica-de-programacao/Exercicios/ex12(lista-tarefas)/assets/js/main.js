f20203e4931d766830ab8ecba86dc4dd962734d3
Erickson Siqueira da Silva <83774878+EricksonSiqueira@users.noreply.github.com>
2021-06-13 18:49:29 -0300

    Adicionando tarefa + botão de apagar

--- Logica-de-programacao/Exercicios/ex12(lista-tarefas)/assets/js/main.js
+++ Logica-de-programacao/Exercicios/ex12(lista-tarefas)/assets/js/main.js
@@ -1,5 +1,26 @@
 const botaoAdd = document.querySelector('.adicionar')
+const txtAdcionar = document.querySelector('.add-tarefa-txt')
+const lista = document.querySelector('.tarefas')
+let tarefas = []
+let botoes = []
+let contTarefas = 0
 
+// botão adicionar
 botaoAdd.addEventListener('click', function (e){
-    console.log('pegou')
+    adicionaTarefa()
 })
+
+function adicionaTarefa () {
+    // criando o elemento do txt
+    tarefas.push(document.createElement('li'))
+    tarefas[contTarefas].innerHTML = `${txtAdcionar.value}`
+    tarefas[contTarefas].classList.add(`txt-${contTarefas}`)
+    // criando o botão de apagar ao lado do txt
+    botoes.push(document.createElement('button'))
+    botoes[contTarefas].innerHTML = 'Apagar'
+    botoes[contTarefas].classList.add(`botao-${contTarefas}`)
+    // adicionando a lista
+    lista.appendChild(tarefas[contTarefas])
+    tarefas[contTarefas].appendChild(botoes[contTarefas])
+    contTarefas += 1
+}
