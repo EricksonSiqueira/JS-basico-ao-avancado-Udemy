557311f09642c4c136516b73471ba371eb1e1ed7
Erickson Siqueira da Silva <83774878+EricksonSiqueira@users.noreply.github.com>
2021-06-15 00:26:57 -0300

    Com arrow function agora

--- 3-Funções(avançado)/Treino/funcao/funcao-9(celsius-para-fahrenheit).js
+++ 3-Funções(avançado)/Treino/funcao/funcao-9(celsius-para-fahrenheit).js
@@ -1,3 +1,6 @@
 // Criar uma função que converta uma temperatura de Celsius para Fahrenheit 
 function conversorCelsiusFahrenheit (celsius) {return (celsius*1.8)+32}
 console.log(conversorCelsiusFahrenheit(68))
+
+const conversor = (celsius) => {return (celsius*1.8) + 32}
+console.log(conversor(68)) 
