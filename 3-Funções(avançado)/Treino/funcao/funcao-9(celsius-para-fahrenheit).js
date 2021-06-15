df7f8d85bf5d336ecab00865581df0f9f36050b3
Erickson Siqueira da Silva <83774878+EricksonSiqueira@users.noreply.github.com>
2021-06-15 00:23:01 -0300

    Convertendo graus Celsius para Fahrenheit

---
+++ 3-Funções(avançado)/Treino/funcao/funcao-9(celsius-para-fahrenheit).js
@@ -0,0 +1,3 @@
+// Criar uma função que converta uma temperatura de Celsius para Fahrenheit 
+function conversorCelsiusFahrenheit (celsius) {return (celsius*1.8)+32}
+console.log(conversorCelsiusFahrenheit(68))
