b0cf29a7a3dbb58f6442cc1cb2bcb4b3c0478b45
Erickson Siqueira da Silva <83774878+EricksonSiqueira@users.noreply.github.com>
2021-06-16 14:28:16 -0300

    Função construtora
    Atributos/Métodos privados e públicos. Declaração de função construtora com new. Iniciar função construtora com letra maiúscula sempre.

---
+++ 3-Funções(avançado)/Aulas/aula-47(constructor function)/teoria.js
@@ -0,0 +1,25 @@
+// Função construtora -> objetos
+// Função fabrica -> objetos
+// Consrtutora -> Pessoa (new)
+function Pessoa (nome, sobrenome) {
+    // Privadas
+    const ID = 123456;
+
+    const metodoInterno = () => {
+        console.log('É um metodo interno')
+    }
+
+    // Publicos
+    //this. deixa o metodo ou atribúto público
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+
+    this.metodo = () => {
+        console.log(this.nome + ': sou um método')
+    };
+}
+// new -> cria um objeto vazio e faz o this apontar pro objeto vazio e retorna a variável
+const p1 = new Pessoa('Erickson', 'Siqueira')
+const p2 = new Pessoa('Anna', 'Paula')
+console.log (p1.sobrenome)
+p1.metodoInterno()
