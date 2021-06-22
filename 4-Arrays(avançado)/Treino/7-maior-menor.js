318344be03291b9dc74ea05075e9747eaed64c52
Erickson Siqueira da Silva <83774878+EricksonSiqueira@users.noreply.github.com>
2021-06-22 14:03:54 -0300

    Maior e menor com reduce

--- 4-Arrays(avançado)/Treino/7-maior-menor.js
+++ 4-Arrays(avançado)/Treino/7-maior-menor.js
@@ -1,27 +1,28 @@
 const numeros = []
 const indiceMaior = []
 const indiceMenor = []
-let maior = 0
-let menor = 0
-
+//gerando 5 numeros
 for(let i = 0; i < 5; i++){
     numeros.push(Math.round(Math.random()*10))
     console.log(numeros[i])
 }
 
-/*for(let i = 0; i < numeros.length; i++){
-    if(i === 0){
-        maior = numeros[i]
-        menor = numeros [i]
-        indiceMaior.push(i)
-        indiceMenor.push(i)
-    }
-} */
-maior = numeros.reduce(function (a, b) {
+let maior = numeros.reduce(function (a, b) {
     return Math.max(a, b)
 })
-menor = numeros.reduce(function(a, b){
+let menor = numeros.reduce(function(a, b){
     return Math.min(a, b)
 })
-console.log('Maior valor: ',maior)
-console.log('Menor valor: ',menor)
+
+for(let i = 0; i < numeros.length; i++){
+    if(numeros[i] === maior){
+        indiceMaior.push(i)
+    }
+    if(numeros[i] === menor){
+        indiceMenor.push(i)
+    }
+}
+
+console.log(`Maior valor: ${maior} / Indices: ${indiceMaior}`)
+console.log(`Maior valor: ${menor} / Indices: ${indiceMenor}`)
+
