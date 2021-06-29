e4b5b0d04bd39316b68ff2f14d2746c5d7b3c454
Erickson Siqueira da Silva <83774878+EricksonSiqueira@users.noreply.github.com>
2021-06-29 13:31:42 -0300

    Condição dupla, ter sobrenome Silva e ser Menor de 18

--- 4-Arrays(avançado)/Treino/18-filter.js
+++ 4-Arrays(avançado)/Treino/18-filter.js
@@ -1,16 +1,17 @@
 const pessoas = [
-    {nome: 'Erickson', sobrenome: 'Siqueira da Silva', idade: 23},
+    {nome: 'Erickson', sobrenome: 'Siqueira da Silva', idade: 16},
     {nome: 'Anna', sobrenome: 'Paula Souza de Santos', idade: 20},
     {nome: 'Ana', sobrenome: 'Lúcia de Melo Siqueira da Silva', idade: 51},
-    {nome: 'Ladimir', sobrenome: 'dos Santos da Silva', idade: 55},
+    {nome: 'Ladimir', sobrenome: 'dos Santos da Silva', idade: 13},
     {nome: 'Rogério', sobrenome: 'Pereira Pontes', idade: 17},
     {nome: 'Marco', sobrenome: 'Antônio Braga Junior', idade: 24},
     {nome: 'Samuel', sobrenome: 'Silva de Brito', idade: 23},
-    {nome: 'Roberta', sobrenome: 'Silveira Pontes', idade: 14},
+    {nome: 'Ana', sobrenome: 'Silveira Pontes', idade: 14},
 ]
 
 const pessoasMaioresDe18 = pessoas.filter(obj => obj.idade >= 18)
 const pessoasMenoresDe18 = pessoas.filter(obj => !(obj.idade >= 18))
-const sobrenomeSilva = pessoas.filter(obj => obj.sobrenome.toLowerCase().indexOf('silva') > -1)
+const sobrenomeSilva = pessoas.filter(obj => obj.sobrenome.toLowerCase().indexOf('silva') > -1 )
 const sobrenomePontes = pessoas.filter(obj => obj.sobrenome.toLowerCase().indexOf('pontes') > -1)
-console.log(sobrenomePontes)
+const sobrenomeSilvaMenoresDe18 = pessoas.filter(obj => obj.sobrenome.toLowerCase().indexOf('silva') > -1 && obj.idade < 18)
+console.log(sobrenomeSilvaMenoresDe18)
