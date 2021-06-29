949cded338703e4fe4eda059686bb2f83ca744e3
Erickson Siqueira da Silva <83774878+EricksonSiqueira@users.noreply.github.com>
2021-06-29 13:28:33 -0300

    Pessoas com o sobrenome Pontes

--- 4-Arrays(avançado)/Treino/18-filter.js
+++ 4-Arrays(avançado)/Treino/18-filter.js
@@ -3,7 +3,7 @@ const pessoas = [
     {nome: 'Anna', sobrenome: 'Paula Souza de Santos', idade: 20},
     {nome: 'Ana', sobrenome: 'Lúcia de Melo Siqueira da Silva', idade: 51},
     {nome: 'Ladimir', sobrenome: 'dos Santos da Silva', idade: 55},
-    {nome: 'Rogério', sobrenome: 'Pereira pontes', idade: 17},
+    {nome: 'Rogério', sobrenome: 'Pereira Pontes', idade: 17},
     {nome: 'Marco', sobrenome: 'Antônio Braga Junior', idade: 24},
     {nome: 'Samuel', sobrenome: 'Silva de Brito', idade: 23},
     {nome: 'Roberta', sobrenome: 'Silveira Pontes', idade: 14},
@@ -12,3 +12,5 @@ const pessoas = [
 const pessoasMaioresDe18 = pessoas.filter(obj => obj.idade >= 18)
 const pessoasMenoresDe18 = pessoas.filter(obj => !(obj.idade >= 18))
 const sobrenomeSilva = pessoas.filter(obj => obj.sobrenome.toLowerCase().indexOf('silva') > -1)
+const sobrenomePontes = pessoas.filter(obj => obj.sobrenome.toLowerCase().indexOf('pontes') > -1)
+console.log(sobrenomePontes)
