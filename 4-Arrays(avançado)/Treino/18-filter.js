f8e0201d06f7d1edcc02d5c241eb58a6f15ebd4e
Erickson Siqueira da Silva <83774878+EricksonSiqueira@users.noreply.github.com>
2021-06-29 13:24:06 -0300

    Maiores e Menores de 18

---
+++ 4-Arrays(avançado)/Treino/18-filter.js
@@ -0,0 +1,13 @@
+const pessoas = [
+    {nome: 'Erickson', sobrenome: 'Siqueira da Silva', idade: 23},
+    {nome: 'Anna', sobrenome: 'Paula Souza de Santos', idade: 20},
+    {nome: 'Ana', sobrenome: 'Lúcia de Melo Siqueira da Silva', idade: 51},
+    {nome: 'Ladimir', sobrenome: 'dos Santos da Silva', idade: 55},
+    {nome: 'Rogério', sobrenome: 'Pereira pontes', idade: 17},
+    {nome: 'Marco', sobrenome: 'Antônio Braga Junior', idade: 24},
+    {nome: 'Samuel', sobrenome: 'Silva de Brito', idade: 23},
+    {nome: 'Roberta', sobrenome: 'Silveira Pontes', idade: 14},
+]
+
+const pessoasMaioresDe18 = pessoas.filter(obj => obj.idade >= 18)
+const pessoasMenoresDe18 = pessoas.filter(obj => !(obj.idade >= 18))
