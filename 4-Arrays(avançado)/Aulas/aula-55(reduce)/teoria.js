7b9457bcc5e142bfcb234cac35e6d446bd1d9e08
Erickson Siqueira da Silva <83774878+EricksonSiqueira@users.noreply.github.com>
2021-07-02 17:52:36 -0300

    Reduce como se fosse map (não recomendado)

--- 4-Arrays(avançado)/Aulas/aula-55(reduce)/teoria.js
+++ 4-Arrays(avançado)/Aulas/aula-55(reduce)/teoria.js
@@ -8,8 +8,13 @@ const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
 //     return acumulador
 // }, 0)
 // console.log(total)
-const pares = numeros.reduce(function (acumulador, valor, indice, array)  {
-    if(valor % 2 === 0) acumulador.push(valor);
+// const pares = numeros.reduce(function (acumulador, valor, indice, array)  {
+//     if(valor % 2 === 0) acumulador.push(valor);
+//     return acumulador
+// },[])
+// console.log(pares)
+const dobro = numeros.reduce(function (acumulador, valor, indice, array)  {
+    acumulador.push(valor*2)
     return acumulador
-},[])
-console.log(pares)
+}, [])
+console.log(dobro)
