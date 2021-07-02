9352c0fd145ec96fbe9abc3e85d5c47f8eafb503
Erickson Siqueira da Silva <83774878+EricksonSiqueira@users.noreply.github.com>
2021-07-02 17:56:16 -0300

    Forma correta de se utilizar reduce.

--- 4-Arrays(avançado)/Aulas/aula-55(reduce)/teoria.js
+++ 4-Arrays(avançado)/Aulas/aula-55(reduce)/teoria.js
@@ -3,18 +3,31 @@
 // Retorne um array com o dobro dos valores (Map)
 
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+//Forma ideial de se usar o reduce
 // const total = numeros.reduce(function (acumulador, valor, indice, array) {
 //     acumulador += valor
 //     return acumulador
 // }, 0)
 // console.log(total)
+
+//Usando como se fosse filter
 // const pares = numeros.reduce(function (acumulador, valor, indice, array)  {
 //     if(valor % 2 === 0) acumulador.push(valor);
 //     return acumulador
 // },[])
 // console.log(pares)
-const dobro = numeros.reduce(function (acumulador, valor, indice, array)  {
-    acumulador.push(valor*2)
+
+//Usando como se fosse map
+// const dobro = numeros.reduce(function (acumulador, valor, indice, array)  {
+//     acumulador.push(valor*2)
+//     return acumulador
+// }, [])
+// console.log(dobro)
+
+const somaPar = numeros.reduce(function(acumulador, valor, indice, array)  {
+    if(valor % 2 === 0){
+        acumulador += valor
+    }
     return acumulador
-}, [])
-console.log(dobro)
+}, 0)
+console.log(somaPar)
