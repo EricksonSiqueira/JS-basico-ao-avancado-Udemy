3878fb37de7f3506766cb73590c364bd31c21d1e
Erickson Siqueira da Silva <83774878+EricksonSiqueira@users.noreply.github.com>
2021-07-02 17:50:18 -0300

    Utilizando o reduce como se fosse filter (não recomendado)

--- 4-Arrays(avançado)/Aulas/aula-55(reduce)/teoria.js
+++ 4-Arrays(avançado)/Aulas/aula-55(reduce)/teoria.js
@@ -3,8 +3,13 @@
 // Retorne um array com o dobro dos valores (Map)
 
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
-const total = numeros.reduce(function (acumulador, valor, indice, array) {
-    acumulador += valor
+// const total = numeros.reduce(function (acumulador, valor, indice, array) {
+//     acumulador += valor
+//     return acumulador
+// }, 0)
+// console.log(total)
+const pares = numeros.reduce(function (acumulador, valor, indice, array)  {
+    if(valor % 2 === 0) acumulador.push(valor);
     return acumulador
-}, 0)
-console.log(total)
+},[])
+console.log(pares)
