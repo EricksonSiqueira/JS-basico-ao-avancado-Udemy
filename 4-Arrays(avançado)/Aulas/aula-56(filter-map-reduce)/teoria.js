a9ab7cbd74cfbdd180849bc107f54859363d9bd6
Erickson Siqueira da Silva <83774878+EricksonSiqueira@users.noreply.github.com>
2021-07-05 19:59:53 -0300

    Somando todos os valores do array

--- 4-Arrays(avançado)/Aulas/aula-56(filter-map-reduce)/teoria.js
+++ 4-Arrays(avançado)/Aulas/aula-56(filter-map-reduce)/teoria.js
@@ -6,4 +6,4 @@
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 11, 15, 22, 27];
 const numerosPares = numeros.filter(valor => valor % 2 === 0)
 const dobrarValores = numeros.map(valor => valor*2)
-console.log(dobrarValores)
+const reduzido = numeros.reduce((somatorio, valor) => somatorio += valor)
