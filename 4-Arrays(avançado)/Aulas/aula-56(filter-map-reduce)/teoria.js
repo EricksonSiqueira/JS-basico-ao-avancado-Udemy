0ee0180eb12e1e23740de018cb560d8c7f0ed15b
Erickson Siqueira da Silva <83774878+EricksonSiqueira@users.noreply.github.com>
2021-07-05 19:57:21 -0300

    Filtrando números pares

---
+++ 4-Arrays(avançado)/Aulas/aula-56(filter-map-reduce)/teoria.js
@@ -0,0 +1,8 @@
+// Retorna a soma do dobro de todos os pares
+// -> Filtrar pares
+// -> Dobrar os valores
+// -> Reduzir (soma tudo)
+
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 11, 15, 22, 27];
+const numerosPares = numeros.filter(valor => valor % 2 === 0)
+console.log(numerosPares)
