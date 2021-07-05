de1f3118c615de4cc2841c0a625ed3fedd6cf845
Erickson Siqueira da Silva <83774878+EricksonSiqueira@users.noreply.github.com>
2021-07-05 19:58:17 -0300

    Dobrando valores com o map

--- 4-Arrays(avançado)/Aulas/aula-56(filter-map-reduce)/teoria.js
+++ 4-Arrays(avançado)/Aulas/aula-56(filter-map-reduce)/teoria.js
@@ -5,4 +5,5 @@
 
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 11, 15, 22, 27];
 const numerosPares = numeros.filter(valor => valor % 2 === 0)
-console.log(numerosPares)
+const dobrarValores = numeros.map(valor => valor*2)
+console.log(dobrarValores)
